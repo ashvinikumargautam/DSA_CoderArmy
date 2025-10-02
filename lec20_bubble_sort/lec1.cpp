20990b48ab72fb9a25a0d1b8fdf0b740c2f3f2d8
ashvinikumargautam <ashvini1275@gmail.com>
2025-10-02 11:23:32 +0530

    a

---
+++ lec20_bubble_sort/lec1.cpp
@@ -0,0 +1,20 @@
+#include   <iostream>
+using namespace std;
+int main(){
+    int arr[10] = {29,10,14,37,13,5,7,8,9,1};
+    int n = 10;
+    for (int i=n-2;i>=0;i--){
+       
+        for(int j=0;j<=i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                
+            }
+           
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+}
